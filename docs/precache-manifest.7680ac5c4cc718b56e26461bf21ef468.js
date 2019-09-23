b34af55024dd96ff9bffb6f6bc8a08fb5496c7c4
svad <v.stukalov@traceair.net>
2019-09-23 20:11:43 +0300

    Fix publicDir path

---
+++ docs/precache-manifest.7680ac5c4cc718b56e26461bf21ef468.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed8ba6677e8bb03d3af67f41c1861616",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9528c50c818024f187cb",
+    "url": "./static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f644b01572cf65cef3ae",
+    "url": "./static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "9528c50c818024f187cb",
+    "url": "./static/js/2.e4e3a3c2.chunk.js"
+  },
+  {
+    "revision": "f644b01572cf65cef3ae",
+    "url": "./static/js/main.44e106d3.chunk.js"
+  },
+  {
+    "revision": "9d035edaa9049898bd77",
+    "url": "./static/js/runtime-main.45024ea5.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "docs/precache-manifest.9e914ccb292c4bf670be8d2355bb082b.js"
+  "./precache-manifest.7680ac5c4cc718b56e26461bf21ef468.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("docs/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
